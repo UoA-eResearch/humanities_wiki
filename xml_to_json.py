f1f42e80277348892bd6b792098e2283d9682638
Nick Young <nick.young@auckland.ac.nz>
2017-03-31 11:44:28 +1300

    init

---
+++ xml_to_json.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import json
+import sys
+from pprint import pprint
+
+files = sys.argv[1:]
+
+for filename in files:
+  with open(filename) as f:
+    xml = BeautifulSoup(f.read())
+    versions = []
+    for page in xml.find_all("page"):
+      guid = page.find("guid").string
+      links = {}
+      total_links = 0
+      for version in page.find_all("version"):
+        name = version.find("name").string
+        dt = version.find("createdon").string
+        ct = version.find("contenttext").string
+        if ct:
+          ct = BeautifulSoup(ct)
+          for link in ct.find_all("a", href=True):
+            href = link['href']
+            index = href.find("page_guid=")
+            if index > 0:
+              linked_guid = href[index + len("page_guid="):]
+              pair_id = guid + linked_guid
+              if pair_id not in links:
+                links[pair_id] = {"id": pair_id, "to": linked_guid, "from": guid, "width": 0}
+                links[pair_id]['width'] += 1
+                total_links += 1
+        versions.append({"guid": guid, "links": links, "total_links": total_links, "created_on": dt, "title": name});
+  with open(filename.replace("xml", "json"), "w") as f:
+    json.dump(versions, f)
